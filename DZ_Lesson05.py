feb5cc603625f2d2204a8a249dfebe8547a372e2
«anna» <LovveeGit@ro.ru>
2023-03-31 09:34:20 +0300

    Task06

--- DZ_Lesson05.py
+++ DZ_Lesson05.py
@@ -1,39 +1,28 @@
 #       Задача 6
 
-# list_text = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBB"
-# list = [i for i in list_text]
+list_text = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBB"
+list = [i for i in list_text]
 
-# def RLE(string):
-#     if not all(symmm.isupper() for symmm in string):
-#         raise 
-#     if not string:
-#         return ""
-#     current_symmm = string[0]
-#     current_count = 1
-#     result = ""
-#     for symmm in string[1:]:
-#         if symmm == current_symmm:
-#             current_count += 1
-#         else:
-#             result += current_symmm + str(current_count)
-#             current_symmm = symmm
-#             current_count = 1
-#     result += current_symmm + str(current_count)
-#     return result
-# print(RLE(list_text))
+def RLE(string):
+    if not all(symmm.isupper() for symmm in string):
+        raise 
+    if not string:
+        return ""
+    current_symmm = string[0]
+    current_count = 1
+    result = ""
+    for symmm in string[1:]:
+        if symmm == current_symmm:
+            current_count += 1
+        else:
+            result += current_symmm + str(current_count)
+            current_symmm = symmm
+            current_count = 1
+    result += current_symmm + str(current_count)
+    return result
+print(RLE(list_text))
 
 #       Задача 3
-
-# list_text = ["eat", "tea", "tan", "ate", "nat", "bat"]
-
-# allress = []
-
-# for w in list_text:
-#     group = [w2 for w2 in list_text if set(w2) == set(w)]
-#     if group not in allress:
-#         allress += [group]
-
-# print(allress)
 
 list_text = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
---
+++ DZ_Lesson06.py
@@ -0,0 +1,19 @@
+
+#       Задача 30
+
+
+# a1 = int(input('Введите число: '))
+# d = int(input('Введите число: '))
+# n = int(input('Введите число: '))
+# for i in range(n):
+#     print(a1 + i * d)
+
+
+#       Задача 32
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
